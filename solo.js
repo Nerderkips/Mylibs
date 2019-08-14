d90b2a4ef2c3680025538e93fa8014b847202f2a
Nerderkips <53907622+Nerderkips@users.noreply.github.com>
2019-08-14 13:27:37 +0200

    Create solo.js

---
+++ solo.js
@@ -0,0 +1,13 @@
+//Created by Nerderkips
+
+function getProfile(num){
+
+    return "https://www.sololearn.com/Profile/"+num+"/?ref=app";
+
+}
+
+function getPicture(num){
+
+    return "https://api.sololearn.com/Uploads/Avatars/"+num+".jpg";
+
+}
