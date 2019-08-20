47efab2666defa812b47d1e06d6d7055d6082ddb
Nerderkips <53907622+Nerderkips@users.noreply.github.com>
2019-08-20 08:08:23 +0200

    Update testlang.js

--- testlang.js
+++ testlang.js
@@ -1,6 +1,11 @@
 function print(content){
-  let a = document.createElement("<p>");
 
-  let p document.querySelector("body").appendChild(a);
-  p.html = content;
-} 
+      let a = document.createElement("p");
+
+      let t = document.createTextNode(content);
+
+      a.appendChild(t);
+
+      document.querySelector("body").appendChild(a);
+
+}
